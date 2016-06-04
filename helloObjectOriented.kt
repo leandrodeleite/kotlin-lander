6fa3448245a7c0c2acdc5c39eae59438ce792418
leandrodeleite <leandrodeleite@gmail.com>
2016-06-03 21:56:30 -0300

    Comment application 'hello', Object Oriented

--- helloObjectOriented.kt
+++ helloObjectOriented.kt
@@ -9,3 +9,11 @@ fun main(args: Array<String>) {
     Greeter(args[0]).greet()
 }
 
+/*
+    Class with a primary contructor and
+    a member function
+
+    Note there's no
+    'new' keyword used to create an object
+
+*/
