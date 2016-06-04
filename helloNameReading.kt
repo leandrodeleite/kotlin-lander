2caf56de60f585c7ff626a533db7ff98c542d748
leandrodeleite <leandrodeleite@gmail.com>
2016-06-03 23:24:33 -0300

    Fix application 'hello', Name Reading

--- helloNameReading.kt
+++ helloNameReading.kt
@@ -1,15 +1,21 @@
 
 fun main(args: Array<String>) {
-    if (args.size() == 0) {
-        println("Provide a name")
+    if (args.size == 0) {
+        println("Please provide a name as a command-line argument")
         return
     }
     println("Hello, ${args[0]}!")
 }
 
-// error: expression 'size' of type 'Int' cannot be invoked as a function. The function 'invoke()' is not found
-// if (args.size() == 0) {
-//          ^
+/**
+ *
+ * error: expression 'size' of type 'Int' cannot be invoked as a function. The function 'invoke()' is not found
+ * if (args.size() == 0) {
+ *          ^
+ * FIX:
+ * if (args.size == 0) {
+ *          ^
+ */
 
 /*
     String interpolation to cut down ceremony
