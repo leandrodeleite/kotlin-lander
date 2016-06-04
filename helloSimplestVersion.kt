140ee91225833a8ea8736a70d5fb5b0121ee41c2
leandrodeleite <leandrodeleite@gmail.com>
2016-06-03 21:59:58 -0300

    Comment application 'hello', Simple Version

--- helloSimplestVersion.kt
+++ helloSimplestVersion.kt
@@ -5,3 +5,14 @@ fun main(args: Array<String>) {
     println("Hello World!")
 }
 
+/*
+    Optional package header
+
+    Package-level function,
+    which returns Unit and
+    takes an Array of String as a parameter
+
+    Have you noticed?
+    Semicolons are optional
+
+*/
