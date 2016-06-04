416ff3b16140f4aa2e5a8591f7c3f32306eda1ce
leandrodeleite <leandrodeleite@gmail.com>
2016-06-03 23:42:56 -0300

    Comment application 'hello', Many Names Reading

--- helloManyNamesReading.kt
+++ helloManyNamesReading.kt
@@ -4,4 +4,10 @@ fun main(args: Array<String>) {
         println("Hello, $name!")
 }
 
+/*
+    Demonstrates the for-loop,
+        that would have been called "enhanced" if there were any other for-loop in Kotlin
+        
+    basic-syntax #using-a-for-loop
 
+*/
