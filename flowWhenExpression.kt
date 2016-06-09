1993b6ba5713041e6c3af8b8d0404127dfcdc1d2
leandrodeleite <leandrodeleite@gmail.com>
2016-06-08 22:08:23 -0300

    Turn Kotlin 'when' to an expression

--- flowWhenExpression.kt
+++ flowWhenExpression.kt
@@ -2,7 +2,26 @@
 fun main(args: Array<String>) {
 
 
-    val mode: Int = 10
+    val mode: Int = 2
+
+
+    val result = when (mode) {
+        1 -> "The mode is lazy."
+        2 -> {
+            "The mode is two" +
+            // warning: the expression is used
+            "So the mode is busy."
+            // println("from the expression")
+            // val i: Int = 3
+        }
+        3 -> "The mode is super-productive."
+        else -> "I don't know that mode."
+    }
+
+    println("");
+    println(result)
+    println("");
+
 
 
     when (mode) {
@@ -14,6 +33,7 @@ fun main(args: Array<String>) {
         3 -> println("The mode is super-productive.")
         else -> println("I don't know that mode.")
     }
+
 
 
 }
