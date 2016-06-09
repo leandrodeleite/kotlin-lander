4821e7af6ac0d63b4bcb0644a3cf49f6a0469ef7
leandrodeleite <leandrodeleite@gmail.com>
2016-06-09 20:08:02 -0300

    Test 'Challenge: Conditionals' 'when' ranges

--- flowConditionals.kt
+++ flowConditionals.kt
@@ -10,8 +10,10 @@ fun main(args: Array<String>) {
         in 11..20 -> println("Range from 11 to 20")
         in 21..30 -> println("Range from 21 to 30")
         in 31..40 -> println("Range from 31 to 40")
-        else      -> println("Over 40")             // it gets wrong when random < 0 
-        // random > 40 -> println("Above 40") // 
+        else      -> println("Over 40")             // it gets wrong when random <= 0 
+
+//        random > 40 -> println("Above 40")        // error: incompatible types
+//        (> 40) -> println("Over 40")              // error: expecting expression/'->'
     }
 }
 
