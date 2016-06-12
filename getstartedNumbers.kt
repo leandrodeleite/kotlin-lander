01d6e48a41eecb3bf63fe106f13bc64f4f3ff567
leandrodeleite <leandrodeleite@gmail.com>
2016-06-11 21:14:22 -0300

    Experiment Kotlin application 'Getting Started' Numbers, interpolation

--- getstartedNumbers.kt
+++ getstartedNumbers.kt
@@ -4,9 +4,16 @@ fun main(args: Array<String>) {
     val numbers = arrayListOf(1, 2, 3, 4, 5, 6)
     println(numbers)
 
-    println(numbers.filter { it % 2 == 0 } )
+    val even = numbers.filter { it % 2 == 0 }
 
-    println(numbers.filter { it % 2 == 1 } )
+    val odd = numbers.filter { it % 2 == 1 }
+ 
+
+    println("Even numbers are: $even")
+    println("Odd numbers are:  $odd")
+
+    println("Even numbers are: ${even}")
+    println("Odd numbers are:  ${odd}")
 
 }
 
