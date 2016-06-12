059fb4670ae565eca6700fb21c7e1eea677f816b
leandrodeleite <leandrodeleite@gmail.com>
2016-06-11 21:33:53 -0300

    Create Kotlin application 'Getting Started' Customer, class

---
+++ getstartedCustomer.kt
@@ -0,0 +1,10 @@
+
+    class Customer(val name: String, val email: String) //  Customer@1234abcd
+
+fun main(args: Array<String>) {
+
+    val customer = Customer("John Smith", "john.smith@somewhere.com")
+    println(customer)
+
+}
+
