5a66dfbf24e7879b53aac5edb888f82106dae072
leandrodeleite <leandrodeleite@gmail.com>
2016-06-15 23:37:11 -0300

    Create Script function, type of parameters/result

---
+++ functions.kts
@@ -0,0 +1,16 @@
+
+/*
+    Kotlin function definitions
+    look very much like
+    mathematical function definitions
+
+    f: ZxZ->Z, f(a,b) = a+b
+
+    Indicates type of each parameter of the function as well as the type of the result.
+*/
+
+    fun f(a: Int, b:Int): Int = a + b
+
+    println( f(3 , 9) )
+
+
