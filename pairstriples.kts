6639ed778596a2e7064db7b199310b0fadc58c51
leandrodeleite <leandrodeleite@gmail.com>
2016-06-16 23:19:29 -0300

    Test Script, Pairs 'unpack'

--- pairstriples.kts
+++ pairstriples.kts
@@ -5,4 +5,12 @@ println(a)
 println("a.first " + a.first)
 println("a.second " + a.second)
 
+// "unpack", parentheses required
+
+val (x, y) = a
+
+// println(x)
+// println(y)
+// error: unresolved reference: ...
+
 
