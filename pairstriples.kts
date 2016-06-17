2949a637263df7ae836d2c3aa7e927285b2190ee
leandrodeleite <leandrodeleite@gmail.com>
2016-06-17 00:20:38 -0300

    Test Script, Pairs 'unpacker' function

--- pairstriples.kts
+++ pairstriples.kts
@@ -12,5 +12,22 @@ val (x, y) = a
 // println(x)
 // println(y)
 // error: unresolved reference: ...
+//
+// println($x)
+// println($y)
+// error: ...
 
+val b = Pair(2, 7.9)
+
+println(b)
+println("b.first " + b.first)
+println("b.second " + b.second)
+
+// "unpack" works correctly inside functions
+fun unpacker(b: Pair<Int, Double>) {
+    val (x, y) = b
+    println("$b consists of $x and $y")
+}
+
+unpacker(b)
 
