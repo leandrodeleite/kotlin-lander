a2d7244fb67aeef109d13e4c5de1cd184448ca66
leandrodeleite <leandrodeleite@gmail.com>
2016-06-19 18:05:26 -0300

    Code Script, mastermind, add prints

--- mastermind.kts
+++ mastermind.kts
@@ -9,7 +9,15 @@ fun createSecret() {
 // main game
 fun main() {
     val secret = createSecret()
-    
+    val history = Array<String>(MaxNumGuesses) { "" }
+    val current = 0
+
+    println("Welcome to Mastermind!")
+    println("I have created a secret combination:")
+    println("Four distinct letters from A - F.")
+    println("You haba $MaxNumGuesses guesses to find it.")
+
+  
 }
 
 main()
