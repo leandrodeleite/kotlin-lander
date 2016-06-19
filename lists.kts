743453785248979409fb294d44335744e90e21ae
leandrodeleite <leandrodeleite@gmail.com>
2016-06-18 23:58:07 -0300

    Test Script, parameterized type

--- lists.kts
+++ lists.kts
@@ -8,5 +8,18 @@ println(L.size)
 println(L[0])
 println(L[3])
 
+println()
+
+for (e in L)
+    println(e)
+
+println()
+
+println(L.lastIndex)
+
+// list is a List<string>
+// List is a parameterized type
+// type of the objects stored inside the list
+
 
 
